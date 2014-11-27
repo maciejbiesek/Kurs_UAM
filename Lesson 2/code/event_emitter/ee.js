15436f12f3cf1c241c20be20a2de1b5b4f5ac44a
maciejbiesek <maciejbiesek@gmail.com>
2014-11-27 22:57:36 +0100

    homeworks

--- Lesson 2/code/event_emitter/ee.js
+++ Lesson 2/code/event_emitter/ee.js
@@ -7,7 +7,7 @@
 
 	EE = function () {
 		this.listeners = {};
-	};
+	}
 
 	EE.prototype.on = function (eventName, listener, context) {
 		if (!this.listeners[eventName]) {
@@ -17,16 +17,16 @@
 		var tempArray = {
 			listener: listener, 
 			context: context
-		}; 
+		}
 
-		this.listeners[eventName] = this.listeners[eventName].concat(tempArray);
+		this.listeners[eventName].push(tempArray);
 		var index = this.listeners[eventName].length - 1;
 		
 		var th = this;
 		return function () {
 			delete th.listeners[eventName][index];
-		};
-	};
+		}
+	}
 
 	EE.prototype.emit = function (eventName) {
 		var args = Array.prototype.slice.call(arguments, 1);
@@ -34,9 +34,9 @@
 		
 		for (var elem in listeners) {
 			listeners[elem].listener.apply(listeners[elem].context, args);
-		};
-	};
+		}
+	}
 
 	global.UAM.EventEmitter = EE;
 
-}(window));
+}(window))
--- Lesson 5/code/js/store.js
+++ Lesson 5/code/js/store.js
@@ -1,9 +1,47 @@
 UAM.Store = function () {
 	UAM.EventEmitter.call(this);
 	this.data  = [];
-};
+	this.active = 0;
+
+	this.findByName = function (data) {
+		for (var i = 0; i < this.data.length; i++) {
+	    	if (this.data[i].name === data) {
+	      		return i;
+	    	}
+  		}
+	}
+}
 
 UAM.utils.inherits(UAM.EventEmitter, UAM.Store);
 
-UAM.Store.prototype.add = function (data) {};
-UAM.Store.prototype.update = function (id, data) {};
+UAM.Store.prototype.update = function (data) {
+	var that = this;
+	var index = this.findByName(data);
+	console.log(this.data[index].state)
+	if (this.data[index].state) {
+		this.data[index].state = 0;
+		this.active--;
+	}
+	else {
+		this.data[index].state = 1;
+		this.active++;
+	}
+	this.emit("activeElements", this.active);
+
+}
+
+UAM.Store.prototype.add = function (data) {
+	var flag = 1;
+	for (var i = 0; i < this.data.length; i++) {
+		if (this.data[i].name === data) {
+			flag = 0;
+			break;
+		}
+	}
+	if (flag) {
+		this.data.push({ name: data, state: 0 });
+		this.emit("addToView", data);
+		this.emit("elementAdded", data);
+	}
+	else alert("This task is already on list!")
+}
